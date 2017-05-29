6d3c846087dd279c940745b6effb5fb781f34bf5
Chytrvs <marcinchytrzynski@gmail.com>
2017-05-29 11:15:12 +0200

    initial commit

---
+++ Assets/EnemyHandle.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHandle : MonoBehaviour {
+    public Material[] Colors;
+    int Score;
+    public Text ScoreTxt;
+    public GameObject Panel;
+    public GameObject Enemy;
+    public Vector3 StartingPos;
+	// Use this for initialization
+	void Start () {
+        StartingPos = transform.position;
+        ScoreTxt.text = Score.ToString();
+        
+        GetComponent<Renderer>().material = Colors[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (GetComponent<Renderer>().material.color == col.gameObject.GetComponent<Renderer>().material.color)
+        {
+        int num = Random.Range(0, 6);
+        GetComponent<Renderer>().material = Colors[num];
+        Score++;
+            if (Score == 10)
+            {
+                Instantiate(this.gameObject, StartingPos, Quaternion.identity);
+            }
+        ScoreTxt.text = Score.ToString();
+        }
+        else
+        {
+            Score = 0;
+            ScoreTxt.text = Score.ToString();
+            Panel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        Application.LoadLevel(0);
+        Time.timeScale = 2;
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    Quaternion StartRotation;
+    Quaternion EndRotation;
+    public float time=0;
+    public float rotationSpeed=1;
+	// Use this for initialization
+	void Start () {
+        /* StartRotation = transform.localRotation;
+         EndRotation = new Quaternion(0, 0, 0,1);
+         EndRotation.SetEulerAngles(0, 0, transform.rotation.ToEulerAngles().z+(Mathf.PI/2));*/
+        QualitySettings.antiAliasing = 8;
+        StartRotation = transform.localRotation;
+        EndRotation = StartRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TapInputHandle();
+        transform.rotation = Quaternion.Slerp(StartRotation, EndRotation, time*rotationSpeed);
+        time += Time.deltaTime;
+	}
+    public void RotateLeft()
+    {
+        StartRotation = transform.localRotation;
+        EndRotation = new Quaternion(0, 0, 0, 1);
+        EndRotation.SetEulerAngles(0, 0, transform.rotation.ToEulerAngles().z + (Mathf.PI / 3));
+        time = 0;
+    }
+    public void RotateRight()
+    {
+        StartRotation = transform.localRotation;
+        EndRotation = new Quaternion(0, 0, 0, 1);
+        EndRotation.SetEulerAngles(0, 0, transform.rotation.ToEulerAngles().z - (Mathf.PI / 3));
+        time = 0;
+    }
+    void TapInputHandle()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch tap = Input.GetTouch(0);
+            if (tap.phase == TouchPhase.Began)
+            {
+                if (tap.position.x < Screen.width / 2)
+                {
+                    RotateLeft();
+                }
+                else if (tap.position.x >= Screen.width/2 )
+                {
+                    RotateRight();
+                }
+
+            }
+
+        }
+    }
+}
